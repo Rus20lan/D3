f21bce3886f560b2adb86045583030cfed955865
Ruslan <ruslas@yandex.ru>
2025-04-04 00:40:10 +0300

    update chapter_03

--- chapter_03/js/main.js
+++ chapter_03/js/main.js
@@ -5,6 +5,15 @@
       .attr('viewBox', '0 0 1200 1600')
       .style('border','1px solid black');
 
-
-
+  d3.csv("data/data.csv", d => {
+    return {
+      technology: d.technology,
+      count: +d.count
+    }
+  }).then(data => {
+    console.log('Максимальное значение', d3.max(data, d => d.count));
+    console.log('Минимальное значение', d3.min(data, d => d.count));
+    console.log('Минимальное и максимальное значение в массиве', d3.extent(data, d => d.count));
+    return data.sort((a,b) => b.count - a.count) // сортировка по убыванию
+  }).then(console.log)
 })();
